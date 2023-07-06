03406562eee4fb1a2bc2c4ac3169bada50f15c71
dave-hickman <dave.robert.hickman@gmail.com>
2023-07-06 14:15:29 +0100

    remove default readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import Image from 'next/image'
 import SearchBar from './SearchBar'
 
+import Script from 'next/script'
+
 export default function Home() {
   return (
+    <>
+    <Script src="https://js.api.here.com/v3/3.1/mapsjs-core.js"/>
+    <Script src="https://js.api.here.com/v3/3.1/mapsjs-service.js"/>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1>Hello</h1>
@@ -10,5 +15,6 @@ export default function Home() {
         <p>This is my project</p>
       </div>
     </main>
+    </>
   )
 }
