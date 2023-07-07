ad3d0750602ca4d29f6e3fefcb293a5f23c816bc
dave-hickman <dave.robert.hickman@gmail.com>
2023-07-07 14:52:18 +0100

    amend nav title

--- src/app/Nav.tsx
+++ src/app/Nav.tsx
@@ -1,7 +1,7 @@
 export default function Nav() {
     return (
         <nav className="bg-gradient-to-tr from-indigo-500 to-pink-500 flex items-center justify-center h-20 w-full fixed z-10">
-        <h1 className=" text-slate-100 text-center text-xl">EV Charging Point Finder</h1>
+        <h1 className=" text-slate-100 text-center text-xl">EV CHARGING POINT FINDER</h1>
         </nav>
     )
 }
