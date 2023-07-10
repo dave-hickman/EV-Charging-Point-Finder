c1aebaee24843b3166c38d5be8770642cda2532f
dave-hickman <dave.robert.hickman@gmail.com>
2023-07-10 10:00:46 +0100

    remove comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,6 @@ export default function Home() {
   return (
     <>
       <main className="flex w-full min-h-screen flex-col items-center justify-between">
-        {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex mt-24">
-          <h1>Hello</h1>
-          <SearchBar />
-          <p>This is my project</p>
-        </div> */}
         <Map apikey={apikey} zoomLevel={zoomLevel} />
       </main>
     </>
